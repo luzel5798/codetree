188ec37a36866a8bddbc36f9d06a85c82744f87c
luzel5798 <73620073+luzel5798@users.noreply.github.com>
2026-02-27 19:41:37 +0900

    Title: 달에서 무게 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260227/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,6 @@
+weight = 13
+ratio = 0.165
+
+print("%d * %.6f = %.6f" %(weight, ratio, weight*ratio))
+#print("{0} * {1:.6f} = {2:.6f}".format(weight, ratio, weight*ratio))
+#print(f"{weight} * {ratio:.6f} = ")
